0f33d249130656576afc802148f35eff9a6fd585
Xelios Zukto <97667708+xszo@users.noreply.github.com>
2024-10-18 15:35:09 +0000

    up net dns

--- src/net/dumps/clash.py
+++ src/net/dumps/clash.py
@@ -63,11 +63,11 @@ MISC = {
 
 
 def misc(src: dict, res: dict) -> None:
-    res["dns"]["default-nameserver"] = [item + ":53" for item in src["misc"]["dns"]]
+    res["dns"]["default-nameserver"] = deepcopy(src["misc"]["dns"])
     if "doh" in src["misc"]:
-        res["dns"]["nameserver"] = [src["misc"]["doh"]]
+        res["dns"]["nameserver"] = src["misc"]["doh"]
     else:
-        res["dns"]["nameserver"] = deepcopy(res["dns"]["default-nameserver"])
+        res["dns"]["nameserver"] = deepcopy(src["misc"]["dns"])
 
 
 def node(src: dict, res: dict) -> None:
